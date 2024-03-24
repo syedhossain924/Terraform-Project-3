13e4dc088e3c7fcb1b2bfde49a700394f658fdce
EC2 Default User <syedhossain249@gmail.com>
2024-03-24 06:50:36 +0000

    terraform main file

--- main2.tf
+++ main2.tf
@@ -15,5 +15,13 @@ resource "aws_instance" "RHEL-Server" {
     volume_type = "gp3"
     encrypted   = true
   }
- 
+resource "aws_instance" "Ubuntu-Linux" {
+  count         = 1
+  ami           = "ami-07d9b9ddc6cd8dd30"
+  instance_type = "t2.micro"
+  key_name      = "Triage_KeyPair"
+  tags = {
+    Name = "Ubuntu-Server"
+  }
+} 
 }
